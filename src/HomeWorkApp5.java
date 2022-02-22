8917c35be50d11351d6548e49f2629e37697b56e
IrinaK <irina_koryakina@mail.ru>
2022-02-22 16:58:49 +0500

    Lesson-4

--- src/HomeWorkApp5.java
+++ src/HomeWorkApp5.java
@@ -1,16 +1,41 @@
+/**
+ * JavaLevel1.Lesson5
+ *  * @author IrinaK
+ *  * @version 22.02.2022
+ */
 class HomeWorkApp5 {
     public static void main(String[] args) {
         Employee[] emplArray = new Employee[5];
-        emplArray[0] = new Employee("Миронов Андрей Петрович", "Директор", "mirra789@mail.ru",
-                                    "8-919-339-70-61", 25000, 56);
-        emplArray[1] = new Employee("Урюпова Надежда Ивановна", "Главный бухгалтер",
-                                    "uni44@mail.ru", "8-982-351-65-14", 20000, 41);
-        emplArray[2] = new Employee("Корякина Ирина Владимировна", "Старший инженер",
-                                    "irina_koryakina@mail.ru", "8-902-869-55-00", 18000, 48);
-        emplArray[3] = new Employee("Андронов Михаил Васильевич", "Инженер",
-                                    "andropov_micha@mail.ru", "8-902-869-66-90", 15000, 36);
-        emplArray[4] = new Employee("Кононов Владимир Вениаминович", "Помощник инженера",
-                                    "masterok@mail.ru", "8-904-339-03-20", 10000, 21);
+        emplArray[0] = new Employee("Миронов Андрей Петрович",
+                                    "Директор",
+                                    "mirra789@mail.ru",
+                                    "8-919-339-70-61",
+                                    25000,
+                                    56);
+        emplArray[1] = new Employee("Урюпова Надежда Ивановна",
+                                    "Главный бухгалтер",
+                                    "uni44@mail.ru",
+                                    "8-982-351-65-14",
+                                    20000,
+                                    37);
+        emplArray[2] = new Employee("Корякина Ирина Владимировна",
+                                    "Старший инженер",
+                                    "irina_koryakina@mail.ru",
+                                    "8-902-869-55-00",
+                                    18000,
+                                    48);
+        emplArray[3] = new Employee("Андронов Михаил Васильевич",
+                                    "Инженер",
+                                    "andropov_micha@mail.ru",
+                                    "8-902-869-66-90",
+                                    15000,
+                                    46);
+        emplArray[4] = new Employee("Кононов Владимир Вениаминович",
+                                    "Помощник инженера",
+                                    "masterok@mail.ru",
+                                    "8-904-339-03-20",
+                                    10000,
+                                    21);
         for (Employee empl : emplArray) {
             if (empl.getAge() > 40) {
                 System.out.println(empl);
