6a757d4617efc7286ff0d7c73e0563991ee0cabd
IrinaK <irina_koryakina@mail.ru>
2022-02-22 17:16:22 +0500

    Lesson-5

--- src/HomeWorkApp5.java
+++ src/HomeWorkApp5.java
@@ -6,36 +6,46 @@
 class HomeWorkApp5 {
     public static void main(String[] args) {
         Employee[] employeesArray = new Employee[5];
-        employeesArray[0] = new Employee("Миронов Андрей Петрович",
+        employeesArray[0] = new Employee(
+                "Миронов Андрей Петрович",
                 "Директор",
                 "mirra789@mail.ru",
                 "8-919-339-70-61",
                 25000,
-                56);
-        employeesArray[1] = new Employee("Урюпова Надежда Ивановна",
+                56
+        );
+        employeesArray[1] = new Employee(
+                "Урюпова Надежда Ивановна",
                 "Главный бухгалтер",
                 "uni44@mail.ru",
                 "8-982-351-65-14",
                 20000,
-                37);
-        employeesArray[2] = new Employee("Корякина Ирина Владимировна",
+                37
+        );
+        employeesArray[2] = new Employee(
+                "Корякина Ирина Владимировна",
                 "Старший инженер",
                 "irina_koryakina@mail.ru",
                 "8-902-869-55-00",
                 18000,
-                48);
-        employeesArray[3] = new Employee("Андронов Михаил Васильевич",
+                48
+        );
+        employeesArray[3] = new Employee(
+                "Андронов Михаил Васильевич",
                 "Инженер",
                 "andropov_micha@mail.ru",
                 "8-902-869-66-90",
                 15000,
-                46);
-        employeesArray[4] = new Employee("Кононов Владимир Вениаминович",
+                46
+        );
+        employeesArray[4] = new Employee(
+                "Кононов Владимир Вениаминович",
                 "Помощник инженера",
                 "masterok@mail.ru",
                 "8-904-339-03-20",
                 10000,
-                21);
+                21
+        );
         for (Employee employee : employeesArray) {
             if (employee.getAge() > 40) {
                 System.out.println(employee);
